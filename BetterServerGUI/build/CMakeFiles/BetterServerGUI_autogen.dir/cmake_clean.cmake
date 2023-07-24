35e131a005b23986587c6ae8733c694598ec1ab3
hander <you@example.com>
2023-07-24 16:26:47 +0200

    Old Linux support

--- BetterServerGUI/build/CMakeFiles/BetterServerGUI_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "BetterServerGUI_autogen/mocs_compilation.cpp"
-  "CMakeFiles/BetterServerGUI_autogen"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/BetterServerGUI_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
